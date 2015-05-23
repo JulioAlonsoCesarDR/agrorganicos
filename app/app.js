9eb8410316a9c8a267bb88de25f39f7f7874cb49
JulioAlonsoCesarDR <jac.dr1013@gmail.com>
2015-05-22 21:35:12 -0500

    add files

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ angular.module('agrorganicos', ['ngRoute', 'ui.bootstrap'])
 
    $routeProvider
    .when('/', {
+<<<<<<< HEAD
         templateUrl: '/agrorganicos/app/views/home/home.html',
         controller: 'homeController'
     })
@@ -25,6 +26,21 @@ angular.module('agrorganicos', ['ngRoute', 'ui.bootstrap'])
     }).otherwise({redirectTo:'/'});
 
    //$locationProvider.html5Mode(true).hashPrefix('!');
+=======
+        templateUrl: 'app/views/home/home.html',
+        controller: 'homeController'
+    })
+   .when('/nosotros', {
+        templateUrl: 'app/views/home/nosotros.html',
+        controller: 'nosotrosController'
+    })
+   .when('/tips-y-recetas', {
+        templateUrl: 'app/views/home/tipsyrecetas.html',
+        controller: 'tipsyrecetasController'
+    });
+
+   $locationProvider.html5Mode(true);
+>>>>>>> Added features
 
 }])
 
@@ -59,6 +75,7 @@ angular.module('agrorganicos', ['ngRoute', 'ui.bootstrap'])
 
 })
 
+<<<<<<< HEAD
 .controller('noticiasController', function($scope, $http){
     $http.get('/wp-api/wp-json/posts').success(function (res) {
         $scope.news = res;
@@ -72,6 +89,8 @@ angular.module('agrorganicos', ['ngRoute', 'ui.bootstrap'])
     });
 })
 
+=======
+>>>>>>> Added features
 .controller('ModalInstanceCtrl', function ($scope, $modalInstance, product) {
 
     $scope.product = product;
@@ -114,7 +133,11 @@ angular.module('agrorganicos', ['ngRoute', 'ui.bootstrap'])
                     name: 'Humus',
                     description: 'Es el resultado de la descomposición  de la materia orgánica por la acción digestiva de la lombriz, alguno de sus beneficios son: <ul><li>Mejora de la fertilidad del suelo</li><li>Retiene agua y minerales</li><li> Aporta nitrógeno, fósforo, potasio, magnesio, etc.</li><li>Potencializa el crecimiento de las plantas </li></ul>',
                     color: '-green',
+<<<<<<< HEAD
                     image: 'http://zoraba.mx/agrorganicos/assets/imgs/hummus_back.jpg',
+=======
+                    image: 'assets/imgs/hummus_back.jpg',
+>>>>>>> Added features
                     text: 'Este abono orgánico proviene de la descomposición de materia orgánica por la acción digestiva de la lombriz. Mejora la fertilidad del suelo,  retiene agua y minerales, aporta nitrógeno, fósforo, potasio, magnesio; potencializa el crecimiento de las plantas. Presentación sólida de 2 kg y líquida (lixiviado).',
                     subtitle: 'solido | lixiviado'
                 },
@@ -122,7 +145,11 @@ angular.module('agrorganicos', ['ngRoute', 'ui.bootstrap'])
                     name: 'Nopal',
                     description: 'El consumo del nopal orgánico tiene más beneficios saludables ya que no usa pesticidas y fertilizantes artificales en la producción <ul><li>Los productos orgánicos se diferencian por: </li><li>Mejor sabor y color</li><li> Aporta más minerales y vitaminas</li><li>Protege la salud de consumidores y productores </li></ul>',
                     color: '-orange',
+<<<<<<< HEAD
                     image: 'http://zoraba.mx/agrorganicos/assets/imgs/nopal_back.jpg',
+=======
+                    image: 'assets/imgs/nopal_back.jpg',
+>>>>>>> Added features
                     text:'Producimos nopal orgánico libre de pesticidas y fertilizantes artificiales.Nopal cambray y en salmuera en bolsas de 1 kg.',
                     subtitle: 'cambray | salmuera'
                 },
@@ -130,7 +157,11 @@ angular.module('agrorganicos', ['ngRoute', 'ui.bootstrap'])
                     name: 'Hortalizas',
                     description: 'el consumo de cultivos orgánicos tiene más beneficios saludables ya que no usa pesticidas y fertilizantes artificales en la producción <ul><li>Los productos orgánicos se diferencian por: </li><li>Mejor sabor y color</li><li> Aporta más minerales y vitaminas</li><li>Protege la salud de consumidores y productores </li><li>Pregunta por los productos de temporada</li></ul>',
                     color: '-red',
+<<<<<<< HEAD
                     image: 'http://zoraba.mx/agrorganicos/assets/imgs/hortalizas_back.jpg',
+=======
+                    image: 'assets/imgs/hortalizas_back.jpg',
+>>>>>>> Added features
                     text:'Lombriz roja californiana (Lumbricus rubellus) es la más usada por su reproduccion, alimentación, secreción y docilidad. Favorece la fertilidad de la tierra.Proporciona permeabilidad para el aire y para el agua.Aumenta la retención de agua y la capacidad de almacenar y liberar los nutrientes.',
                     subtitle: ''
                    
@@ -139,7 +170,11 @@ angular.module('agrorganicos', ['ngRoute', 'ui.bootstrap'])
                     name: 'Lombriz',
                     description: 'consulta el calendarios de cusos',
                     color: '-blue',
+<<<<<<< HEAD
                     image: 'http://zoraba.mx/agrorganicos/assets/imgs/lombriz_back.jpg',
+=======
+                    image: 'assets/imgs/lombriz_back.jpg',
+>>>>>>> Added features
                     text:'Nuestros productos orgánicos tienen mejor sabor y color, aportan más minerales y vitaminas.Tenemos desde germinados o plántulas hasta el  producto terminado. Rábano, cebolla, chile serrano, espinaca, lechuga, acelga. Pregunta por los productos de temporada. ',
                     subtitle: 'plántula | producto terminado'
                 }
